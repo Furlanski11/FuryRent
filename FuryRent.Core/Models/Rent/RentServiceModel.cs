ede92433b079040f97b6cc5d65097a9a8909e869
Antoan Furlanski <antoan.furlanski@gmail.com>
2024-03-19 23:17:27 +0200

    Implemented Add and All view for RentController and view models for them

---
+++ FuryRent.Core/Models/Rent/RentServiceModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FuryRent.Core.Models.Rent
+{
+	public class RentServiceModel
+	{
+        public int RentId { get; set; }
+
+        public int CarId { get; set; }
+
+        public DateTime RentalStartDate { get; set; }
+
+		public DateTime RentalEndDate { get; set; }
+	}
+}
---
+++ FuryRent.Core/Models/Rent/RentViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace FuryRent.Core.Models.Rent
+{
+	public class RentViewModel
+	{
+        public int Id { get; set; }
+
+        public int CarId { get; set; }
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public string Make { get; set; } = string.Empty;
+
+        public string Model { get; set; } = string.Empty;
+
+        public string PricePerDay { get; set; } = string.Empty;
+
+        public DateTime RentalStartDate { get; set; }
+
+		public DateTime RentalEndDate { get; set; }
+
+        public string TotalCost { get; set; } = string.Empty;
+	}
+}
